41168b7a8129fa79330ed0096e1dff701ea9345b
selki3 <sophiedillon07@gmail.com>
2024-05-05 23:28:07 +0100

    fix typo

--- baselines/llama2.py
+++ baselines/llama2.py
@@ -72,8 +72,8 @@ def main():
     training_args = TrainingArguments("test-trainer", evaluation_strategy="epoch")
 
     model = GPT2Model.from_pretrained('gpt2')
-    train_dataloader = GPT2Trainer.get_dataloader("train", batch_size=8, shuffle=True)
-    eval_dataloader = GPT2Trainer.get_dataloader("dev", batch_size=8)
+    train_dataloader = GPT2Trainer.train_dataloader()
+    eval_dataloader = GPT2Trainer.val_dataloader()
 
     trainer = Trainer(
         model=model,
