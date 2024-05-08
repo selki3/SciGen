88a44475ab2b37c9b10c0864395992a3b328ea8f
Sophie <sdillon1@sheffield.ac.uk>
2024-05-08 16:54:35 +0100

    return to initial fml.py

--- baselines/fml.py
+++ baselines/fml.py
@@ -1,25 +1,16 @@
 import torch 
-from transformers import AutoTokenizer, TrainingArguments, AutoModelForSeq2SeqLM, Trainer
+from transformers import AutoTokenizer, DataCollatorWithPadding, TrainingArguments, AutoModelForSeq2SeqLM, Trainer, GPT2Tokenizer, GPT2Model, AdamW
 import numpy as np
-import sacrebleu as scb
-from moverscore_v2 import get_idf_dict, word_mover_score
-from torch.utils.data import DataLoader
+import evaluate
 from utils import Table2textFlanDataset
+from huggingface_hub import login
+from torch.utils.data import DataLoader
+from transformers import get_linear_schedule_with_warmup 
 import logging
 
-def create_predictions(model, tokenizer, data):
-    model.eval()
-    preds = []
-    i = 0
-    dataloader = DataLoader(data, batch_size=4)
-    for batch in dataloader:
-        outputs = model.generate(input_ids=batch['input_ids'].to(model.device), attention_mask=batch['attention_mask'].to(model.device))
-        decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        preds.extend(decoded_outputs)
-        print(i)
-        print(len(dataloader))
-        i += 1
-    return preds
+access_token_read = "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"
+access_token_write = "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"
+login(token=access_token_read)
 
 def main():
     tokenizer = AutoTokenizer.from_pretrained('google/flan-t5-base')
@@ -30,57 +21,49 @@ def main():
     model = AutoModelForSeq2SeqLM.from_pretrained('google/flan-t5-base')
     model.resize_token_embeddings(len(tokenizer))
 
+    train_dataset = Table2textFlanDataset(tokenizer, data_dir="../dataset/few-shot", type_path="train", max_source_length=384, max_target_length=384)
+    eval_dataset = Table2textFlanDataset(tokenizer, data_dir="../dataset/few-shot", type_path="dev", max_source_length=384, max_target_length=384)
+
     training_args = TrainingArguments(
         output_dir="test_trainer", 
         evaluation_strategy="epoch",
         per_device_train_batch_size=8,
         per_device_eval_batch_size=8,
         learning_rate=5e-5,
-        num_train_epochs=1,
+        num_train_epochs=10,
         logging_strategy="epoch",
-    )
-    train_dataset = Table2textFlanDataset(tokenizer, data_dir="../dataset/few-shot", type_path="train", max_source_length=384, max_target_length=384)
-    test_dataset = Table2textFlanDataset(tokenizer, data_dir="../dataset/few-shot", type_path="test", max_source_length=384, max_target_length=384)
-    eval_dataset = Table2textFlanDataset(tokenizer, data_dir="../dataset/few-shot", type_path="dev", max_source_length=384, max_target_length=384)
-
+        )
 
     trainer = Trainer(
         model=model,
         args=training_args,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
+
     )
     trainer.train()
 
+    def create_predictions(model, tokenizer, data):
+        model.eval()
+        preds = []
+        dataloader = DataLoader(data, batch_size=8)
+        for batch in dataloader:
+            outputs = model.generate(input_ids=batch['input_ids'].to(model.device), attention_mask=batch['attention_mask'].to(model.device))
+            decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            preds.extend(decoded_outputs)
+        return preds
     
-    def get_references(path):
-        with open(path, 'r') as target:
+    def get_references():
+        with open("../dataset/few-shot/test.target", 'r') as target:
             references = [line.strip() for line in target]
         return [[ref] for ref in references]
     
     test_dataset = Table2textFlanDataset(tokenizer, data_dir="../dataset/few-shot", type_path="test", max_source_length=384, max_target_length=384)
     preds = create_predictions(model, tokenizer, test_dataset)
-    refs = get_references("../dataset/few-shot/test.target")   
-    bleu = scb.corpus_bleu(preds, [refs])
-    with open("predictions.txt", "w") as file:
-        for line in preds:
-	        file.write(line)
-	
-
-    idf_dict_hyp = get_idf_dict(preds)
-    idf_dict_ref = get_idf_dict(refs)
-
-    scores = word_mover_score(refs, preds, idf_dict_ref, idf_dict_hyp, \
-                        stop_words=[], n_gram=1, remove_subwords=True, batch_size=64)
-    moverscore_mean = np.mean(scores)
-    moverscore_median = np.median(scores)
-
-    print(f"BLEU Scores: {bleu.score}")
-    print(f"MoverScore Mean: {moverscore_mean}, Median: {moverscore_median}")
-   
-    with open("evaluation_scores.txt", "w") as file:
-        file.write(f"BLEU Scores: {bleu.score}\n")
-        file.write(f"MoverScore Mean: {moverscore_mean}, Median: {moverscore_median}\n")
+    refs = get_references()   
+    bleu = evaluate.load('bleu')
+    bleu_score = bleu.compute(predictions=preds, references=refs)
+    print(f"BLEU: {bleu_score}")
 
 
 if __name__ == "__main__":
